7935c529bb9ba7dfd98dfa529bd966f9b52e504d
yurikrupnik <krupnik.yuri@gmail.com>
2019-04-20 07:38:08 +0300

    init

---
+++ src/PillButton/PillButton.jsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+import Button from '@material-ui/core/Button';
+
+const mainColor = '#b3d7ee';
+const textColor = '#333';
+
+const StyledButton = styled(Button)`
+    &&: {
+        width: 16px
+        padding: 3.5 0;
+        min-height: 22px;
+        font-size: 11px;
+        border-radius: 5px;
+        
+        &:hover {
+            color: ${mainColor};        
+            background: ${textColor};        
+        }
+    }   
+`;
---
+++ src/index.js
@@ -0,0 +1,4 @@
+// import Buttons from './Buttons';
+export default from 'Buttons';
+export default from 'Avatar';
+export default from 'PillButton';
